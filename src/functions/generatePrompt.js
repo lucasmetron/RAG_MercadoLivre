173d4ca1ed9fcd9dd9baa466906f49f905140746
Lucas Rosa <lucasmetron@gmail.com>
2026-05-08 16:55:23 -0300

    projeto pronto

--- src/functions/generatePrompt.js
+++ src/functions/generatePrompt.js
@@ -5,6 +5,8 @@ function returnPrompt(question, context) {
     Responda apenas usando as informações abaixo do contexto.
     Se não souber sobre o assunto responda "Infelizmente não consigo te ajudar"
     Se perguntar algo fora do escolpo ou o contexto estiver vázio responder "Não posso dar opnião sobre assuntos externos"
+    Mantenha um tom profissional e amigavel
+    
 
     CONTEXTO:
       ${context}
